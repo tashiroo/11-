0516d4e92401b44bab00434d7a59232b70122da9
taashiroo <ing631tom@outlook.jp>
2020-11-13 18:56:04 +0900

    11/13の学習途中経過

---
+++ 1113/1113-3/js/script.js
@@ -0,0 +1,41 @@
+'use strict';
+
+(() => {
+  class Icon{
+    constructor(canvas){
+      this.ctx = canvas.getContext("2d");
+      this.width = canvas.width;
+      this.height = canvas.height;
+      this.r =60;
+   
+  }
+  draw() {
+    this.ctx.translate(this.width /2,this.height /2);
+
+    this.ctx.beginPath();
+    // this.ctx.arc(this.width / 2, this.height /2, this.r,0,2 * Math.PI);
+    this.ctx.arc(0, 0, this.r,0,2 * Math.PI);
+    this.ctx.stroke();
+
+    this.ctx.beginPath();
+    // this.ctx.moveTo(this.width /2,this.height /2 - this.r - 5);
+    // this.ctx.lineTo(this.width /2,this.height /2 - this.r + 5);
+    this.ctx.moveTo(0, - this.r - 5);
+    this.ctx.lineTo(0, - this.r + 5);
+    this.ctx.strokeStyle = "orange";
+    this.ctx.lineWidth =6;
+    this.ctx.stroke();
+  }
+  run(){
+    this.draw();
+  }
+
+  }
+  const canvas = document.querySelector("canvas");
+  if(typeof canvas.getContext === "undefined") {
+    return;
+  }
+
+  const icon = new Icon(canvas);
+  icon.run();
+})();
